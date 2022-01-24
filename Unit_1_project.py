88e5c556ab04191240eac34b7c2a0cbf429398cc
Logan G <lpglendening25@students.bmchs.org>
2022-01-24 09:17:49 -0600

    done with all

---
+++ Unit_1_project.py
@@ -0,0 +1,25 @@
+from curses import init_pair
+from multiprocessing.connection import wait
+from os import wait3
+
+
+print("Hello welcome to Madlibs New York Edition!")
+print(" I will give you nouns, verbs, and adjetives that you will need to fill out.")
+Verb1 = input (" Pick a verb ")
+Verb2 = input (" Pick a verb ")
+Verb3 = input (" Pick a verb ")
+Verb4 = input (" Pick a verb ")
+Verb5 = input (" Pick a verb ")
+Noun1 = input ("Pick a noun ")
+Noun2 = input ("Pick a noun ")
+Noun3 = input ("Pick a noun ")
+Noun4 = input ("Pick a noun ")
+Noun5 = input ("Pick a noun ")
+Noun6 = input ("Pick a noun ")
+Adj1 = input ("Pick a adjitive ")
+Adj2 = input ("Pick a adjitive ")
+Adj3 = input ("Pick a adjitive ")
+Adj4 = input ("Pick a adjitive ")
+print("Your MadLib now is...........................")
+print(" Hello! Welcome and thank you for coming! My name is " + Noun1 + ". And I am your host for this party here at " +  Noun2 + ". Tonight we will be " +  Verb2 + ". *CRASH* Oh no its the " + " " + Adj2 + " " +  Adj2 + " " + Adj3 + " " + Noun3 + " " + Verb3 + ". Oh he is scared by our " +  Verb3 + ". Crawls in to the sewers never to be seen again.  What a " + Adj4 +  " night. As you are walking to your house in New York City you get stoped by a " +  Noun4 + " You " + Verb4 + " for your life. You " + Verb5 + " all the way home.  He now knows where you lives. Everyday he sends you a " + Noun5 + " in the mail. ")
+print( "Thanks for reading")
